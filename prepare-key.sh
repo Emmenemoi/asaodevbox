a634b4d6a9f101a64a9dffd1bee6d63a75eeda7b
Adrien <AdRiLiNuX@users.noreply.github.com>
2020-11-06 18:17:19 +0100

    remove losetup

--- prepare-key.sh
+++ prepare-key.sh
@@ -4,22 +4,24 @@ if [ $(id -u) -ne 0 ]; then
   sudo "$0"
   exit
 fi
-if [ ! -d /mnt/test ]; then
-  mkdir /mnt/test
-fi
 
-apt update && apt install -y wget parted dosfstools udev
+ln -s /usr/share/zoneinfo/Etc/UTC /etc/localtime
+apt update && apt install -y wget parted dosfstools udev cmake libfdisk1 libfdisk-dev libfuse2 libfuse-dev build-essential git
+git clone https://github.com/braincorp/partfs.git
+cd partfs/
+make
+cd ..
 
-mkdir -p livecd/{iso,squashfs}
+mkdir -p livecd/{ubuntu,iso,squashfs} tmp{,2}
 cd livecd/
 if [ ! -f ubuntu-20.04.1-live-server-amd64.iso ]; then
 	echo "No install iso present. Download"
-    wget https://releases.ubuntu.com/20.04/ubuntu-20.04.1-live-server-amd64.iso
+  wget https://releases.ubuntu.com/20.04/ubuntu-20.04.1-live-server-amd64.iso
 fi
-mount ubuntu-20.04.1-live-server-amd64.iso /mnt/test -o loop
+mount ubuntu-20.04.1-live-server-amd64.iso ubuntu -o loop,ro
 echo "Prepare iso partition"
-cp -a /mnt/test/. iso/
-umount /mnt/test
+cp -a ubuntu/. iso/
+umount livecd/ubuntu
 sed \
   '/set timeout=5/ amenuentry "AutoInstall AsaoDevBox Server" {\n\tset gfxpayload=keep\n\tlinux /casper/vmlinuz quiet autoinstall ---\n\tinitrd /casper/initrd\n}' \
   -i iso/boot/grub/grub.cfg
@@ -35,33 +37,36 @@ parted -s -a optimal AsaoDevBox.img unit mib mkpart ESP fat32 1 1g name 1 AsaoDe
 #because cloud-init look for TYPE=vfat or iso9660 or LABEL=CIDATA or LABEL=cidata
 parted -s -a optimal AsaoDevBox.img unit mib mkpart primary fat32 1g 1100 name 2 cidata
 parted -s -a optimal AsaoDevBox.img unit mib mkpart primary fat32 1100 100% name 3 asao-user-data
-losetup -f -P AsaoDevBox.img
+partfs/build/bin/partfs -o dev=AsaoDevBox.img tmp
+
+#losetup -f -P AsaoDevBox.img
 echo "Wait mount AsaoDevBox.img"
-until [ -e /dev/disk/by-partlabel/AsaoDevBox ]; do sleep 1; done
-mkfs.vfat /dev/disk/by-partlabel/AsaoDevBox
-mount /dev/disk/by-partlabel/AsaoDevBox /mnt/test
+#until [ -e /dev/disk/by-partlabel/AsaoDevBox ]; do sleep 1; done
+mkfs.vfat tmp/p1
+mount tmp/p1 tmp2
 echo "Copy ubuntu iso to AsaoDevBox.img"
-cp -r livecd/iso/. /mnt/test/
-umount /mnt/test
-mkfs.vfat -F 32 -n cidata /dev/disk/by-partlabel/cidata
-mount /dev/disk/by-partlabel/cidata /mnt/test
+cp -r livecd/iso/. tmp2/
+umount tmp2
+mkfs.vfat -F 32 -n cidata tmp/p2
+mount tmp/p2 tmp2
 echo "Copy cidata to AsaoDevBox.img"
 for i in user-data meta-data extra-user-data; do
   if [ -e "$i" ]; then
-    cp "$i" /mnt/test/
+    cp "$i" tmp2/
   fi
 done
-umount /mnt/test
-mkfs.vfat -F 32 -n asao-user-data /dev/disk/by-partlabel/asao-user-data
-mount /dev/disk/by-partlabel/asao-user-data /mnt/test
+umount tmp2
+mkfs.vfat -F 32 -n asao-user-data tmp/p3
+mount tmp/p3 tmp2
 echo "Copy userdata to AsaoDevBox.img"
 if [ "x${SUDO_USER}" == "x" ]; then
-  cp ~/.ssh/id_rsa.pub /mnt/test/
+  cp ~/.ssh/id_rsa.pub tmp2/
 else
-  cp /home/${SUDO_USER}/.ssh/id_rsa.pub /mnt/test/
+  cp /home/${SUDO_USER}/.ssh/id_rsa.pub tmp2/
 fi
-umount /mnt/test
-losetup -d $(losetup -j AsaoDevBox.img | cut -d: -f1)
+umount tmp2/
+#losetup -d $(losetup -j AsaoDevBox.img | cut -d: -f1)
+fusermount -u tmp
 
 if [ $# -eq 1 ]; then
 	echo "Write AsaoDevBox.img to key"
